bdcc0928bd1ff796d2901fb073c2fa5c87cd3a4d
oeunkyoung <o3ozzvb@gmail.com>
2026-03-15 14:50:25 +0900

    feat(dispatcher): DispatcherServlet 구현

---
+++ src/main/java/com/diy/framework/web/DispatcherServlet.java
@@ -0,0 +1,39 @@
+package com.diy.framework.web;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+/**
+ * Front Controller 역할
+ * - 모든 HTTP 요청을 가장 먼저 받아 공통 작업을 처리한 후 적절한 컨트롤러로 요청을 위임.
+ */
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        final Map<String, ?> params = parseParams(req);
+
+        // 강의 등록 로직
+        // 강의 목록 로직
+    }
+
+    private Map<String, ?> parseParams(final HttpServletRequest req) throws IOException {
+        if ("application/json".equals(req.getHeader("Content-Type"))) {
+            final byte[] bodyBytes = req.getInputStream().readAllBytes();
+            final String body = new String(bodyBytes, StandardCharsets.UTF_8);
+
+            return new ObjectMapper().readValue(body, new TypeReference<Map<String, Object>>() {});
+        } else {
+            return req.getParameterMap();
+        }
+    }
+}
