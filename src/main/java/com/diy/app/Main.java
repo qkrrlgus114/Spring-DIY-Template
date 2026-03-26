52189cd790e2e3d18672ebe09133a75ef62a5366
oeunkyoung <o3ozzvb@gmail.com>
2026-03-26 23:32:54 +0900

    refactor(app): Main에서 수동 객체 생성을 BeanFactory 기반 DI로 전환
    - BeanFactory를 통해 LectureController 빈 조회
    - 수동 new LectureRepository/LectureController 제거

--- src/main/java/com/diy/app/Main.java
+++ src/main/java/com/diy/app/Main.java
@@ -1,23 +1,25 @@
 package com.diy.app;
 
 import com.diy.framework.web.DispatcherServlet;
+import com.diy.framework.web.beans.factory.BeanFactory;
 import com.diy.framework.web.mvc.controller.Controller;
 import com.diy.framework.web.server.TomcatWebServer;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ 소켓
+ 웹소켓 -> 양방향 통신할 때 사용하는 프로토콜 웹소켓 (소켓이랑 다름)
+ 모든 통신은 소켓열고 한다.
+ port 를 하나 여는 게 = 소켓을 하나 연다.
+ */
 public class Main {
-    public static void main(String[] args) {
-        // 소켓
-        // 웹소켓 -> 양방향 통신할 때 사용하는 프로토콜 웹소켓 (소켓이랑 다름)
-        // 모든 통신은 소켓열고 한다.
-        // port 를 하나 여는 게 = 소켓을 하나 연다.
-        LectureRepository lectureRepository = new LectureRepository();
-        lectureRepository.save(new Lecture(1L, "이것이 자바다"));
-        lectureRepository.save(new Lecture(2L, "스프링 만들기"));
+    public static void main(String[] args) throws Exception {
 
-        Controller lectureController = new LectureController(lectureRepository);
+        BeanFactory beanFactory = new BeanFactory("com.diy");
+
+        LectureController lectureController = beanFactory.getBean(LectureController.class);
 
         Map<String, Controller> handlerMap = new HashMap<>();
         handlerMap.put("GET /lectures", lectureController);
