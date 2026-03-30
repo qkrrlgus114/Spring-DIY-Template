c2d05513cca058f6b1e34a1f7a03fc6d79c2d8f8
oeunkyoung <o3ozzvb@gmail.com>
2026-03-30 20:31:50 +0900

    refactor(app): LectureController에서 @Component 어노테이션 제거

--- src/main/java/com/diy/app/LectureController.java
+++ src/main/java/com/diy/app/LectureController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
 public class LectureController implements Controller {
     private final LectureRepository lectureRepository;
 
